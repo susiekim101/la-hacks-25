8f38529c652f00a61f1c9f6e93b3617fe9738182
froitcake <fionalu05@gmail.com>
2025-04-27 08:21:40 -0400

    git commit

--- frontend/src/components/NextFlightCard.tsx
+++ frontend/src/components/NextFlightCard.tsx
@@ -1,26 +1,33 @@
 import "../css/NextFlightCard.css";
+
 function NextFlightCard() {
   return (
     <div className="next-flight-card">
-      <p>airline</p>
-      <p>flight name</p>
+      <div className="topSection">
+        {/* <img alt="Delta Airlines" className="airlineLogo" /> */}
+        <div className="airlineLogoPlaceholder"></div>
+        <div className="flightInfo">
+          <div className="airlineName">Delta</div>
+          <div className="flightNumber">Flight D1234</div>
+        </div>
+      </div>
 
       <div className="flight-info">
-        <div className="departure">
-          <p>departing flight</p>
-          <p>BOS</p>
-          <p>logan international airport</p>
-          <p>4/26/2025</p>
-          <p>2:31pm</p>
-          <p>Local Time</p>
+        <div className="column">
+          <p className="columnHeading">Departing Flight</p>
+          <p className="airportCode">BOS</p>
+          <p className="airportName">Logan Airport</p>
+          <p className="date"> 4/26/2025</p>
+          <p className="time">2:31pm</p>
+          <p className="timezone">Local Time</p>
         </div>
-        <div className="arrival">
-          <p>arriving flight</p>
-          <p>LAX</p>
-          <p>los angeles international airport</p>
-          <p>4/26/2025</p>
-          <p>5:15pm</p>
-          <p>Local Time</p>
+        <div className="column">
+          <p className="columnHeading">Arriving Flight</p>
+          <p className="airportCode">LAX</p>
+          <p className="airportName">Los Angeles Airport</p>
+          <p className="date">4/26/2025</p>
+          <p className="time">5:15pm</p>
+          <p className="timezone">Local Time</p>
         </div>
       </div>
     </div>
