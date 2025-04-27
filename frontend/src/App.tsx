290cfd777e43cb2ded65e082a7c847fb724a4cf2
michelllinstar <140552879+michelllinstar@users.noreply.github.com>
2025-04-26 20:01:01 -0700

    edit upload file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Opening from './pages/opening.tsx';
 import Login from './pages/login.tsx';
 import SignUp from './pages/signup.tsx';
+import FlightInfo from './pages/flight_info.tsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Opening/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<SignUp/>} />
+        <Route path="/flightinfo" element={<FlightInfo/>} />
       </Routes>
     </BrowserRouter>
   );
